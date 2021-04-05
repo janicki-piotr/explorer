3fda86a4d778a4330448f101d4bfff4564e35c5a
PcPiotr <pcpiotr@gmail.com>
2021-04-05 22:47:21 +0200

    GUI

---
+++ presentation/src/main/java/pl/redny/gw2explorer/ui/settings/SettingsFragment.kt
@@ -0,0 +1,12 @@
+package pl.redny.gw2explorer.ui.settings
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+import pl.redny.gw2explorer.R
+
+class SettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+}
